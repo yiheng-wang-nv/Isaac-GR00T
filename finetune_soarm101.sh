334f4e31c69500271b5bd9dc459d975d764a97d3
Yiheng Wang <vennw@nvidia.com>
2025-06-06 10:32:49 +0000

    update
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>

--- finetune_soarm101.sh
+++ finetune_soarm101.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH=/localhome/local-vennw/code/Isaac-GR00T:$PYTHONPATH
 python scripts/gr00t_finetune.py --dataset-path /localhome/local-vennw/.cache/huggingface/lerobot/Venn/so101_pick_and_place \
 --num-gpus 1 --batch-size 16 --max-steps 10000 \
---data_config so101_wrist --report_to tensorboard --video_backend torchvision_av
+--data_config so101_wrist --report_to tensorboard --video_backend torchvision_av \
 --output_dir "so101_wrist_finetune_pick_and_place"
