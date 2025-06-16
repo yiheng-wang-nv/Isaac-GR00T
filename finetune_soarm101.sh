17755856ecc238b375e98ba9fcc1fde277a12568
Yiheng Wang <vennw@nvidia.com>
2025-06-16 06:27:58 +0000

    update

--- finetune_soarm101.sh
+++ finetune_soarm101.sh
@@ -1,32 +1,38 @@
-# gr00t n1.5
+# gr00t n1.5 finetune
 python scripts/gr00t_finetune.py \
    --dataset-path /localhome/local-vennw/code/Isaac-GR00T/finetune_data/2_cameras_fps15_enhanced_gripper \
    --num-gpus 1 \
-   --batch-size 16 \
-   --output-dir 2_cameras_fps15_enhanced_gripper_finetune \
-   --max-steps 10000 \
+   --batch-size 32 \
+   --output-dir 2_cameras_fps15_enhanced_gripper_finetune_5k_196_data \
+   --max-steps 5000 \
+   --save-steps 1000 \
    --data-config so100_dualcam \
    --report_to tensorboard \
    --video-backend torchvision_av
 
-# check finetune for checkpoint-3000 to 10000
-for i in {10000..10000..1000}; do
-   python scripts/eval_policy.py --plot \
-      --embodiment_tag new_embodiment \
-      --model_path so101_scissors_2_cameras_fps15_finetune/checkpoint-${i} \
-      --data_config so100_dualcam \
-      --dataset_path finetune_data/so101_scissors_2_cameras_fps15 \
-      --video_backend torchvision_av \
-      --modality_keys single_arm gripper
-   echo "Finished checkpoint-${i}"
-done
-
 # deploy
 python scripts/inference_service.py --server \
-    --model_path so101_scissors_2_cameras_fps15_finetune/checkpoint-8000 \
+    --model_path 2_cameras_fps15_enhanced_gripper_finetune_5k_196_data/checkpoint-5000 \
     --embodiment-tag new_embodiment \
     --data-config so100_dualcam \
     --denoising-steps 4
 
-# "Grip a tweezer and put it in the box."
-# "Grip a straight scissor and put it in the box."
+# inference request, scissor
+# sudo chmod 666 /dev/ttyACM1
+
+python eval_gr00t_so101.py \
+   --host 127.0.0.1 \
+   --port 5555 \
+   --camera_index 0 \
+   --port_follower /dev/ttyACM0 \
+   --task_description "Grip a straight scissor and put it in the box." \
+   --actions_to_execute 300
+
+# inference request, tweezer
+python eval_gr00t_so101.py \
+   --host 127.0.0.1 \
+   --port 5555 \
+   --camera_index 0 \
+   --port_follower /dev/ttyACM0 \
+   --task_description "Grip a tweezer and put it in the box." \
+   --actions_to_execute 300
