83249474cb2b1e521f58841d8e924dd9d2a357f3
Yiheng Wang <vennw@nvidia.com>
2025-06-25 07:05:40 +0000

    add

--- finetune_soarm101.sh
+++ finetune_soarm101.sh
@@ -10,6 +10,31 @@ python scripts/gr00t_finetune.py \
    --report_to tensorboard \
    --video-backend torchvision_av
 
+# add more data further finetune
+python scripts/gr00t_finetune.py \
+   --dataset-path /localhome/local-vennw/code/Isaac-GR00T/finetune_data/2_cameras_fps15_enhanced_gripper \
+   --num-gpus 1 \
+   --batch-size 32 \
+   --output-dir 2_cameras_fps15_enhanced_gripper_finetune_30k_196_data \
+   --max-steps 3000 \
+   --save-steps 1000 \
+   --data-config so100_dualcam \
+   --report_to tensorboard \
+   --video-backend torchvision_av \
+   --base_model_path /localhome/local-vennw/code/Isaac-GR00T/2_cameras_fps15_enhanced_gripper_finetune_30k_196_data/checkpoint-30000
+
+# 231 data finetune
+python scripts/gr00t_finetune.py \
+   --dataset-path /localhome/local-vennw/code/Isaac-GR00T/finetune_data/2_cameras_fps15_enhanced_gripper \
+   --num-gpus 1 \
+   --batch-size 32 \
+   --output-dir 2_cameras_fps15_enhanced_gripper_finetune_30k_231_data \
+   --max-steps 30000 \
+   --save-steps 5000 \
+   --data-config so100_dualcam \
+   --report_to tensorboard \
+   --video-backend torchvision_av
+
 # deploy
 python scripts/inference_service.py --server \
     --model_path 2_cameras_fps15_enhanced_gripper_finetune_30k_231_data/checkpoint-30000 \
