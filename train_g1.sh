c649e48c6acc2af3bde8684d24462cd11c76133e
Yiheng Wang <vennw@nvidia.com>
2025-10-16 14:36:55 +0000

    dd

--- train_g1.sh
+++ train_g1.sh
@@ -1,4 +1,4 @@
 python scripts/gr00t_finetune.py \
 --dataset-path /localhome/local-vennw/code/dataset/install_trocar \
- --num-gpus 8 --batch-size 32 --output-dir g1_install_trocar_4gpu_32bs_50k_steps \
- --data-config unitree_g1 --report_to tensorboard --max_steps 50000 --save-steps 10000
+ --num-gpus 4 --batch-size 32 --output-dir g1_install_trocar_4gpu_32bs_50k_steps \
+ --data-config unitree_g1 --video_backend decord --report_to tensorboard --max_steps 50000 --save-steps 10000
