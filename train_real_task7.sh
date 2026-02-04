20f89f1aaa135533be0c1fbf98a8ba61fcb49c0e
Yiheng Wang <vennw@nvidia.com>
2026-02-04 15:20:50 +0000

    example

---
+++ train_real_task7.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate gr00t
+
+
+CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node=1 gr00t/experiment/launch_finetune.py \
+  --base-model-path nvidia/GR00T-N1.6-3B \
+  --dataset-path /localhome/local-vennw/code/orca-template1-dev/task7_20260122_trimmed_with_mask \
+  --embodiment-tag NEW_EMBODIMENT \
+  --num-gpus 1 \
+  --output-dir /localhome/local-vennw/code/Isaac-GR00T/outputs/task7_20260122_trimmed_with_mask \
+  --save-total-limit 5 \
+  --save-steps 5000 \
+  --max-steps 150000 \
+  --tune-visual \
+  --global-batch-size 12 \
+  --dataloader-num-workers 4 --color_jitter_params brightness 0.3 contrast 0.4 saturation 0.5 hue 0.08 \
+  --extra_augmentation_config '{"background_noise_transforms": [{"target_mask_values": [0], "p": 0.95}]}'
+
+
+
+
+python scripts/dump_dataloader_transforms.py \
+  --dataset_path /localhome/local-vennw/code/orca-template1-dev/task7_20260122_trimmed_with_mask \
+  --embodiment_tag NEW_EMBODIMENT \
+  --output_dir /localhome/local-vennw/code/Isaac-GR00T/debug_augment_vis_real \
+  --save_grid \
+  --grid_num_frames 9 \
+  --no_resize \
+  --video_episode_index 0 --extra_augmentation_config '{"background_noise_transforms": [{"target_mask_values": [0], "p": 0.95}]}'
